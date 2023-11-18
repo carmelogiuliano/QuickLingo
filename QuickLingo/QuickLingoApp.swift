b421adf8a88dc8f6db3bf9382104b8e3636de454
Carmelo <carmelo@Carmelos-MacBook-Air.local>
2023-11-18 16:28:12 +1100

    Project init

---
+++ QuickLingo/QuickLingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickLingoApp.swift
+//  QuickLingo
+//
+//  Created by Carmelo on 18/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct QuickLingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
